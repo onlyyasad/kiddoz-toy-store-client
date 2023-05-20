afbf7b97c0aeb1e4ddde63b26cfa38363ecce3bc
onlyyasad <onlyyasad@gmail.com>
2023-05-21 02:11:49 +0600

    readme file added

---
+++ README.md
@@ -0,0 +1,20 @@
+
+# Kiddoz toy store Website
+
+This website is about kids toy cars store.
+
+## Features
+
+- MongoDB used for Database management and CRUD operations implemented.
+- This is a single page application. 
+- Mobile and Desktop Responsive
+- Latest React Router DOM technology
+- Firebase authentication integrated.
+- Express JS integrated for the server side data load and deployed to Vercel.
+- Private/Protected Route integrated so that user's can't visit/see protected route/contents without login/register.
+
+
+
+## Live Website
+
+https://kiddoz-d45a3.web.app/
--- src/Pages/Home/Gallery.jsx
+++ src/Pages/Home/Gallery.jsx
@@ -9,7 +9,7 @@ const Gallery = () => {
     const [loading, setLoading] = useState(true)
 
     useEffect(() =>{
-        fetch("https://assignment-11-server-umber.vercel.app/toys")
+        fetch("https://assignment-11-server-umber.vercel.app/allToys?limit=10")
         .then(res => res.json())
         .then(data => {
             setCars(data)
@@ -17,16 +17,13 @@ const Gallery = () => {
         })
     }, [])
 
-    if(loading){
-        return <div className='h-screen flex justify-center items-center'><BounceLoader color="#36d7b7" /></div>
-    }
 
     console.log(cars)
     return (
         <div className="bg-gradient-to-b from-teal-50 space-y-8">
             <h2 data-aos="fade-up" className="text-3xl font-bold text-center">Toys Gallery</h2>
             <div className="grid grid-cols-2 md:grid-cols-10 gap-4 p-4">
-                {  
+                {  loading ? <div className='h-screen flex justify-center items-center'><BounceLoader color="#36d7b7" /></div> :
                     cars.map(car => <GalleryCard key={car._id} car={car}></GalleryCard>)
                 }
             </div>
